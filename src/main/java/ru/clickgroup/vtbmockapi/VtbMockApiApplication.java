bc96e3cdb32187d6cc1ebe1e49604607f4390428
Andreesian <90260170+Andreesian@users.noreply.github.com>
2023-10-14 01:48:21 +0300

    Костыль для работы с бд

--- src/main/java/ru/clickgroup/vtbmockapi/VtbMockApiApplication.java
+++ src/main/java/ru/clickgroup/vtbmockapi/VtbMockApiApplication.java
@@ -2,8 +2,9 @@ package ru.clickgroup.vtbmockapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class VtbMockApiApplication {
 
     public static void main(String[] args) {
