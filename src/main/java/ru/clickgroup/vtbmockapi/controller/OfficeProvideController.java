6e9ef7d33b189038368fe1b26c3b2e51c16a5ac2
Andreesian <90260170+Andreesian@users.noreply.github.com>
2023-10-15 01:01:11 +0300

    простите я слишком устал чтобы писать коммиты

--- src/main/java/ru/clickgroup/vtbmockapi/controller/OfficeProvideController.java
+++ src/main/java/ru/clickgroup/vtbmockapi/controller/OfficeProvideController.java
@@ -3,7 +3,8 @@ package ru.clickgroup.vtbmockapi.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.geo.Point;
 import org.springframework.web.bind.annotation.*;
 //import ru.clickgroup.vtbmockapi.domain.office.OfficeEntity;
 import ru.clickgroup.vtbmockapi.domain.office.test_models.Office;
@@ -13,31 +14,61 @@ import ru.clickgroup.vtbmockapi.dto.OfficeData;
 import ru.clickgroup.vtbmockapi.dto.OpenHoursData;
 import ru.clickgroup.vtbmockapi.repo.*;
 import ru.clickgroup.vtbmockapi.repo.OfficeRepository;
+import ru.clickgroup.vtbmockapi.services.OfficeService;
 
+import java.util.Comparator;
 import java.util.List;
 
 @RestController
 @RequestMapping("/offices")
 @Tag( name = "OFFICE")
+@RequiredArgsConstructor
 public class OfficeProvideController {
 
-    @Autowired
     private OfficeRepository officeRepository;
 
-    @Autowired
     private OfficeOpenHoursRepository officeOpenHoursRepository;
 
-    @Autowired
     private OfficeCategoryRepository officeCategoryRepository;
 
-    @Autowired
     private OfficeOpenHoursIndividualRepository officeOpenHoursIndividualRepository;
+
+    private final OfficeService officeService;
 
     //    @GetMapping
 //    public List<OfficeEntity> getAllOffices() {
 //        return OfficeRepository.findAll();
 //    }
 //
+    private static final int EARTH_RADIUS = 6371;
+
+    private double calculateDistance(Office office, Point point) {
+        double lat1 = office.getLatitude();
+        double lon1 = office.getLongitude();
+        double lat2 = point.getY();
+        double lon2 = point.getX();
+
+        double dLat = toRadians(lat2 - lat1);
+        double dLon = toRadians(lon2 - lon1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+                + Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2))
+                * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = EARTH_RADIUS * c;
+        //System.out.println("Distance between point and " + atm.getAddress() + " is " + distance);
+        return distance;
+    }
+
+    private double toRadians(double degree) {
+        return degree * Math.PI / 180;
+    }
+    @PostMapping("/closest")
+    @Operation(summary = "Получить ближайшие оффисы", security = @SecurityRequirement(name = "bearerAuth"))
+    public List<Office> getClosestOffices(@RequestBody Point point) {
+        List<Office> offices = officeService.getAllOffices();
+        offices.sort(Comparator.comparingDouble(a -> calculateDistance(a, point)));
+        return offices.subList(0, 10);
+    }
 
     @GetMapping("/")
     @Operation(summary = "Получить все офисы", security = @SecurityRequirement(name = "bearerAuth"))
--- src/main/java/ru/clickgroup/vtbmockapi/services/FalconService.java
+++ src/main/java/ru/clickgroup/vtbmockapi/services/FalconService.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import ru.clickgroup.vtbmockapi.controller.AtmController;
+import ru.clickgroup.vtbmockapi.controller.OfficeProvideController;
 import ru.clickgroup.vtbmockapi.domain.falcon.Query;
 import ru.clickgroup.vtbmockapi.domain.falcon.Response;
 
@@ -26,38 +27,44 @@ public class FalconService {
     @Autowired
     AtmController atmController;
 
+    @Autowired
+    OfficeProvideController officeProvideController;
+
     static String responseTemplate = "answer in the format of json depending on what user wants to do as following template:\n" +
             "{\n" +
             "\"action\":\"\",\n" +
             "\"category\":\"\"\n" +
             "}\n" +
             "action can be only \"find_atm\", \"find_office\" or \"frequently_visited_places\". always default to those closest actions\n" +
-            "category can be only none, blind or qrRead or wheelchair";
+            "category can be only none, blind or qrRead or wheelchair. always default to none.\n";
     private final RestTemplate restTemplate;
     public String getActionAndCategory(Query data, Point point) {
         data.setText(FalconService.responseTemplate + ' ' + data.getText());
-        System.out.println(data);
+        //System.out.println(data);
         HttpEntity<Query> entity = new HttpEntity<>(data);
         ResponseEntity<Response> response = restTemplate.postForEntity(falconEndpointUrl + "/ask", entity, Response.class);
-        System.out.println("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ" + response.getBody().getResult());
+        //System.out.println("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ" + response.getBody().getResult());
         if (response.getStatusCode() == HttpStatus.OK) {
             ObjectMapper objectMapper = new ObjectMapper();
             try {
+                System.out.println(response.getBody().getResult());
                 JsonNode rootNode = objectMapper.readTree(response.getBody().getResult());
                 JsonNode activeNode = rootNode.get("action");
                 if (activeNode != null && activeNode.isTextual()) {
                     String activeValue = activeNode.asText();
-                    System.out.println(activeValue);
+                    //System.out.println(activeValue);
                     switch (activeValue) {
                         case "find_atm":
                             System.out.println();
-                            atmController.getClosestAtms(point);
-                            System.out.println("Выполняется действие для 'actions'.");
+                            System.out.println("Выполняется find_atm.");
+                            return atmController.getClosestAtms(point).toString();
                         case "find_office":
                             // Обработка других значений "active"
-                            System.out.println("Выполняется другое действие.");
+                            System.out.println("Выполняется find_office.");
+                            return officeProvideController.getClosestOffices(point).toString();
                         case "frequently_visited_places":
-                            System.out.println("Неизвестное значение 'active'.");
+                            System.out.println("Выполняется frequently_visited_places.");
+                            break;
                     }
                 } else {
                     System.out.println("Команда не найдена!");
@@ -65,7 +72,7 @@ public class FalconService {
             } catch (JsonProcessingException e) {
                 throw new RuntimeException(e);
             }
-            System.out.println("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ" + response.getBody().getResult());
+            //System.out.println("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ" + response.getBody().getResult());
             return response.getBody().getResult();
         } else {
             throw new RuntimeException("Failed to send data: " + response.getStatusCode());
