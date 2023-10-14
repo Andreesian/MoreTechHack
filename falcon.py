38ab12016dd147528c90abc83dd0dc5fff2c7712
Andreesian <90260170+Andreesian@users.noreply.github.com>
2023-10-14 20:34:35 +0300

    Небольшие поправочки

--- falcon.py
+++ falcon.py
@@ -9,10 +9,6 @@ cookies = sign.login()
 cookie_path_dir = "./cookies_snapshot"
 sign.saveCookiesToDir(cookie_path_dir)
 
-# Load cookies when you restart your program:
-# sign = login(email, None)
-# cookies = sign.loadCookiesFromDir(cookie_path_dir) # This will detect if the JSON file exists, return cookies if it does and raise an Exception if it's not.
-
 # Create a ChatBot
 chatbot = hugchat.ChatBot(cookies=cookies.get_dict())  # or cookie_path="usercookies/<email>.json"
 
