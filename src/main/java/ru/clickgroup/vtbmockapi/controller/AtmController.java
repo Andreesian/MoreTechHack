5f8f21c029e2caf60b956b49df68b2aaa351feca
Andreesian <90260170+Andreesian@users.noreply.github.com>
2023-10-14 21:27:31 +0300

    Ближайший банкомат

--- src/main/java/ru/clickgroup/vtbmockapi/controller/AtmController.java
+++ src/main/java/ru/clickgroup/vtbmockapi/controller/AtmController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import ru.clickgroup.vtbmockapi.domain.atm.AtmEntity;
 import ru.clickgroup.vtbmockapi.services.AtmService;
 
+import java.util.Comparator;
 import java.util.List;
 
 @RestController
@@ -19,11 +20,33 @@ public class AtmController {
 
     private final AtmService atmService;
 
+    private static final int EARTH_RADIUS = 6371;
+
+    private double calculateDistance(AtmEntity atm, Point point) {
+        double lat1 = atm.getLatitude();
+        double lon1 = atm.getLongitude();
+        double lat2 = point.getY();
+        double lon2 = point.getX();
+
+        double dLat = toRadians(lat2 - lat1);
+        double dLon = toRadians(lon2 - lon1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+                + Math.cos(toRadians(lat1)) * Math.cos(toRadians(lat2))
+                * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = EARTH_RADIUS * c;
+        //System.out.println("Distance between point and " + atm.getAddress() + " is " + distance);
+        return distance;
+    }
+
+    private double toRadians(double degree) {
+        return degree * Math.PI / 180;
+    }
     @PostMapping("/closest")
     public List<AtmEntity> getClosestAtms(@RequestBody Point point) {
         List<AtmEntity> atms = atmService.getAllAtms();
-        atms.sort((a1, a2) -> Double.compare(calculateDistance(a1, point), calculateDistance(a2, point)));
-        return atms.subList(0, 10);
+        atms.sort(Comparator.comparingDouble(a -> calculateDistance(a, point)));
+        return atms.subList(0, 1);
     }
 
     @GetMapping
