1938193bc14ed6197cafb4fb0a7ef29443c15965
Andreesian <90260170+Andreesian@users.noreply.github.com>
2023-10-14 20:48:26 +0300

    WIP Ближайший банкомат

--- src/main/java/ru/clickgroup/vtbmockapi/controller/AtmController.java
+++ src/main/java/ru/clickgroup/vtbmockapi/controller/AtmController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.geo.Point;
 import org.springframework.web.bind.annotation.*;
 import ru.clickgroup.vtbmockapi.domain.atm.AtmEntity;
 import ru.clickgroup.vtbmockapi.services.AtmService;
@@ -18,6 +19,12 @@ public class AtmController {
 
     private final AtmService atmService;
 
+    @PostMapping("/closest")
+    public List<AtmEntity> getClosestAtms(@RequestBody Point point) {
+        List<AtmEntity> atms = atmService.getAllAtms();
+        atms.sort((a1, a2) -> Double.compare(calculateDistance(a1, point), calculateDistance(a2, point)));
+        return atms.subList(0, 10);
+    }
 
     @GetMapping
     public List<AtmEntity> getAllAtms() {
